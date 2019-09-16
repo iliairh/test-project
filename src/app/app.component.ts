5747babeeab03f355ad1d88ced77cbd3708e1998
UninstallGame <zdehnP1992>
2019-09-16 22:24:38 +0800

    Кнопка. Сыбытие click. Пример работы с массивом

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,16 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  someone = 'Hello world!';
+  counter = 0;
+  //           0                  1                  2
+  arr = ['Hello world!', 'I Learn JavaScript', 'It\'s easy'];
+
+  click() {
+    if (this.counter > 2) {
+      return;
+    }
+    this.someone = this.arr[this.counter];
+    this.counter++;
+  }
 }
